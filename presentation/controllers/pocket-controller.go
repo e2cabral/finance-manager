60afbe2c4ea4daad2d069859c1c0e6853dfeb411
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-15 18:58:49 -0300

    🚧 add a method to get a pocket by id

--- presentation/controllers/pocket-controller.go
+++ presentation/controllers/pocket-controller.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"finance-manager/domain/services"
 	h "finance-manager/infra/helpers/http"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 type PocketController struct{}
@@ -20,4 +22,22 @@ func (controller *PocketController) GetPockets(w http.ResponseWriter, r *http.Re
 		h.InternalServerError(w, err.Error())
 	}
 	h.Ok(w, &pockets)
+}
+
+func (controller *PocketController) GetPocketById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+
+	service := services.PocketService{}
+
+	pocket, err := service.GetPocketById(uint(id))
+	if err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+
+	h.Ok(w, &pocket)
 }
