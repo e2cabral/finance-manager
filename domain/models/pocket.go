db5632dc99a6d07936237b56e3dc508547f784d9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-15 18:16:54 -0300

    ♻️ change pocket's primary key name

--- domain/models/pocket.go
+++ domain/models/pocket.go
@@ -1,7 +1,7 @@
 package models
 
 type Pocket struct {
-	PocketID    uint   `json:"pocket_id" gorm:"primaryKey"`
+	ID          uint   `json:"id" gorm:"primaryKey"`
 	UserID      uint   `json:"user_id"`
 	Type        string `json:"type"`
 	Name        string `json:"name"`
