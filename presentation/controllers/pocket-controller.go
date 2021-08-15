2d3df9aa2295ddc069c399cf27d04a7c30958c5c
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-15 18:49:43 -0300

    🚧 add a method to get all pockets

--- presentation/controllers/pocket-controller.go
+++ presentation/controllers/pocket-controller.go
@@ -1,7 +1,23 @@
 package controllers
 
+import (
+	"finance-manager/domain/services"
+	h "finance-manager/infra/helpers/http"
+	"net/http"
+)
+
 type PocketController struct{}
 
 func NewPocketController() *PocketController {
 	return &PocketController{}
+}
+
+func (controller *PocketController) GetPockets(w http.ResponseWriter, r *http.Request) {
+	service := services.PocketService{}
+
+	pockets, err := service.GetPockets()
+	if err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+	h.Ok(w, &pockets)
 }
