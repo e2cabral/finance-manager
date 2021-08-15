0f32eb79004439a697c204b2c8b44ca8b175ddb4
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-14 21:23:13 -0300

    🚧 create movement service

---
+++ domain/services/movement-service.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"finance-manager/data/repositories"
+	"finance-manager/domain/models"
+)
+
+type MovementService struct{}
+
+func (m *MovementService) GetMovements() ([]models.Movement, error) {
+	var movements []models.Movement
+
+	repository, err := repositories.NewMovementRepository()
+	if err != nil {
+		return nil, err
+	}
+
+	repository.GetMovements(&movements)
+	return movements, nil
+}
+
+func (m *MovementService) Save(movement models.Movement) (models.Movement, error) {
+	var newMovement models.Movement
+
+	repository, err := repositories.NewMovementRepository()
+	if err != nil {
+		return models.Movement{}, err
+	}
+	repository.Save(&movement)
+	return newMovement, nil
+}
