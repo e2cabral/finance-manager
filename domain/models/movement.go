bb5734b96bda9fdb8fc860f8806010ef4ddf0c64
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-14 21:23:57 -0300

    🚧 create movement entity

---
+++ domain/models/movement.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type Movement struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	Value     float64        `json:"value"`
+	PocketID  uint           `json:"pocket_id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+}
