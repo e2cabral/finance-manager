e8aa1c1f627827f6940d51153686edee9c2adcef
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-15 19:16:01 -0300

    ✨ add a route to update a movement

--- main/routes/movements.go
+++ main/routes/movements.go
@@ -10,4 +10,5 @@ func MovementsRoutes(r *mux.Router) {
 	r.HandleFunc("/movements", controller.GetMovements).Methods("GET")
 	r.HandleFunc("/movements/{id}", controller.GetById).Methods("GET")
 	r.HandleFunc("/movements", controller.Save).Methods("POST")
+	r.HandleFunc("/movements/{id}", controller.Update).Methods("PATCH")
 }
