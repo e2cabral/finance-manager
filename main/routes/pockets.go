8b030a8af43069953aa8e4cd6a1543ea3322604c
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-15 19:23:53 -0300

    ✨ add a route update a pocket

--- main/routes/pockets.go
+++ main/routes/pockets.go
@@ -10,4 +10,5 @@ func PocketRoutes(r *mux.Router) {
 	r.HandleFunc("/pockets", controller.GetPockets).Methods("GET")
 	r.HandleFunc("/pockets/{id}", controller.GetPocketById).Methods("GET")
 	r.HandleFunc("/pockets", controller.Save).Methods("POST")
+	r.HandleFunc("/pockets/{id}", controller.Update).Methods("PATCH")
 }
