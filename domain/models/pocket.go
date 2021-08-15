abb78568ef723dd500b0596ff314859398f61d53
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-15 20:01:31 -0300

    🚧 add timestamp fields at pocket entity

--- domain/models/pocket.go
+++ domain/models/pocket.go
@@ -1,9 +1,17 @@
 package models
 
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
 type Pocket struct {
-	ID          uint   `json:"id" gorm:"primaryKey"`
-	UserID      uint   `json:"user_id"`
-	Type        string `json:"type"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	ID          uint           `json:"id" gorm:"primaryKey"`
+	UserID      uint           `json:"user_id"`
+	Type        string         `json:"type"`
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
 }
