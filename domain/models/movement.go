356739edc39ecec5cb76dc7e46860403f75e399d
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-15 20:21:54 -0300

    🚧 add a foreign key annotation

--- domain/models/movement.go
+++ domain/models/movement.go
@@ -9,7 +9,7 @@ type Movement struct {
 	ID        uint           `json:"id" gorm:"primaryKey"`
 	Value     float64        `json:"value"`
 	PocketID  uint           `json:"pocket_id"`
-	Pocket    Pocket         `json:"pocket" gorm:"references:ID"`
+	Pocket    Pocket         `json:"pocket" gorm:"references:ID;foreignKey:PocketID"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at"`
