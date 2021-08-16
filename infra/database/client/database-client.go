30ec55c3aaa7aec7097c3fe85289aa1ae460c542
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-16 00:51:59 -0300

    🚧 add environmental variables to the connection string

--- infra/database/client/database-client.go
+++ infra/database/client/database-client.go
@@ -2,12 +2,25 @@ package database
 
 import (
 	"finance-manager/domain/models"
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func Connect() (*gorm.DB, error) {
-	connectionString := "host=localhost user=youruser password=yourpassword dbname=yourdb port=5432 sslmode=disable"
+	password := os.Getenv("DB_PASSWORD")
+	username := os.Getenv("DB_USERNAME")
+	//defaultDb := os.Getenv("DEFAULT_DB")
+	host := os.Getenv("DB_HOST")
+	table := os.Getenv("DB_TABLE")
+	port := os.Getenv("DB_PORT")
+
+	connectionString := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, username, password, table, port,
+	)
+
 	database, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 
 	if err != nil {
