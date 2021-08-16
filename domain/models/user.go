2b5a60f62b23fd2828a8493743ae16cb3b357665
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-16 01:45:49 -0300

    🚧 add relationship between pocket and user

--- domain/models/user.go
+++ domain/models/user.go
@@ -1,7 +1,8 @@
 package models
 
 type User struct {
-	ID       uint   `json:"id" gorm:"primaryKey"`
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID       uint     `json:"id" gorm:"primaryKey"`
+	Username string   `json:"username"`
+	Password string   `json:"password"`
+	Pockets  []Pocket `json:"pockets" gorm:"foreignKey:UserID"`
 }
