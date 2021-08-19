517a02bf8980075fe0dbbb6c795c5100ae87bed9
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-18 23:39:40 -0300

    🚧 add a method to parse data from json

--- infra/helpers/json/json-parser.go
+++ infra/helpers/json/json-parser.go
@@ -2,6 +2,7 @@ package helpers
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 )
@@ -10,4 +11,12 @@ func ToJSON(w http.ResponseWriter, data interface{}) {
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func FromJSON(r io.ReadCloser) (*interface{}, error) {
+	var data interface{}
+	if err := json.NewDecoder(r).Decode(&data); err != nil {
+		return nil, err
+	}
+	return &data, nil
 }
