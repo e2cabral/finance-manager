4fdf99b2c87ee74f211729ee694381af6e32d783
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-18 23:10:00 -0300

    🚧 add a save method to the user controller

--- presentation/controllers/users-controller.go
+++ presentation/controllers/users-controller.go
@@ -1,3 +1,26 @@
 package controllers
 
+import (
+	"encoding/json"
+	"finance-manager/domain/models"
+	"finance-manager/domain/services"
+	h "finance-manager/infra/helpers/http"
+	"net/http"
+)
+
 type UsersController struct{}
+
+func (controller *UsersController) Save(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+
+	service := services.UsersService{}
+	newUser, err := service.Save(user)
+	if err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+
+	h.Ok(w, newUser)
+}
