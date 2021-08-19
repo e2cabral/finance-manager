9e04c7d97ed7a4d9e576a5997cd62be889e84bcb
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-18 23:15:52 -0300

    🚧 add an updated method to the user controller

--- presentation/controllers/users-controller.go
+++ presentation/controllers/users-controller.go
@@ -5,7 +5,9 @@ import (
 	"finance-manager/domain/models"
 	"finance-manager/domain/services"
 	h "finance-manager/infra/helpers/http"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 type UsersController struct{}
@@ -23,4 +25,25 @@ func (controller *UsersController) Save(w http.ResponseWriter, r *http.Request) 
 	}
 
 	h.Ok(w, newUser)
+}
+
+func (controller *UsersController) Update(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+
+	service := services.UsersService{}
+	updated, err := service.Update(uint(id), user)
+	if err != nil {
+		h.InternalServerError(w, err.Error())
+	}
+
+	h.Ok(w, updated)
 }
