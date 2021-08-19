0c03cdaa6ebb31f58341056e2268898546d693e6
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-18 22:53:12 -0300

    🚧 add an update method to the user service

--- domain/services/users-service.go
+++ domain/services/users-service.go
@@ -16,3 +16,13 @@ func (u *UsersService) Save(user models.User) (*models.User, error) {
 	repository.Save(&user)
 	return &user, nil
 }
+
+func (u *UsersService) Update(id uint, user models.User) (*models.User, error) {
+	repository, err := repositories.NewUsersRepository()
+	if err != nil {
+		return nil, err
+	}
+
+	repository.Update(id, &user)
+	return &user, nil
+}
