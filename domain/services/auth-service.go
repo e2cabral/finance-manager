78b5b4cca96030a5e91a286910f3270ce9ce2f4c
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-08-20 01:09:34 -0300

    ♻️ add a signup method to the auth service

--- domain/services/auth-service.go
+++ domain/services/auth-service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"finance-manager/data/repositories"
 	"finance-manager/domain/models"
+	"finance-manager/infra/helpers/encryption"
 )
 
 type AuthService struct{}
@@ -14,6 +15,41 @@ func (a *AuthService) Login(username string, password string) (*models.User, err
 		return nil, err
 	}
 
-	repository.GetUserByUsernameAndPassword(username, password, &user)
+	hashedPassword, err := encryption.Encrypt(password)
+	if err != nil {
+		return nil, err
+	}
+
+	repository.GetUserByUsernameAndPassword(username, hashedPassword, &user)
 	return &user, nil
+}
+
+func (a *AuthService) SignUp(user models.User) (*models.User, error) {
+	repository, err := repositories.NewUsersRepository()
+	if err != nil {
+		return nil, err
+	}
+
+	user.Password, err = encryption.Encrypt(user.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	repository.Save(&user)
+	return &user, nil
+}
+
+func (a *AuthService) IsUsernameUsed(username string) (bool, error) {
+	var user models.User
+	repository, err := repositories.NewUsersRepository()
+	if err != nil {
+		return false, err
+	}
+
+	repository.GetUserByUsername(username, &user)
+
+	if &user != nil {
+		return true, nil
+	}
+	return false, nil
 }
